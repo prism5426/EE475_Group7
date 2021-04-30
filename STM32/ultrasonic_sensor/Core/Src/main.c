f9355e2402447c4b5c61e9ad5af9e467d9d8e8ad
Collin May <mayco@uw.edu>
2021-04-30 16:33:36 -0700

    Bring back ITM debug output

--- STM32/ultrasonic_sensor/Core/Src/main.c
+++ STM32/ultrasonic_sensor/Core/Src/main.c
@@ -58,7 +58,9 @@ static void MX_I2C1_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+int __io_putchar(int ch) {
+	ITM_SendChar(ch);
+}
 /* USER CODE END 0 */
 
 /**
